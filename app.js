e06af84bf181189c8d4f57e95cc9e34ed7b44141
Niklas Gruener <niki.gruener@gmail.com>
2021-09-19 13:02:51 +0200

    Added server files and restructured directories.

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.listen(port, function () {
+  console.log("Server started on port: " + port);
+});
