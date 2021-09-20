84a1a9df06a5870517323e323d8061b0adb491b9
Niklas Gruener <niki.gruener@gmail.com>
2021-09-20 11:18:38 +0200

    Added Logic and finished Desktop App

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -7,7 +7,16 @@ let percentage = 0;
 $(".btn").on("click", function(event) {
   $(".btn").removeClass("btn_clicked")
   $(this).addClass("btn_clicked");
-  percentage = Number($(this).text());
+  if(!($(this).hasClass("btn_custom"))){
+    percentage = Number($(this).text().replace("%", ""));
+  }
+
+  calculate();
+});
+
+$(".btn.btn_custom").on("input", function(event) {
+  percentage = Number($(this).val());
+  calculate();
 });
 
 
@@ -19,9 +28,53 @@ $("#num_people_input").on("input", function(event) {
   }
 });
 
+$("#bill_input").on("input", function(event) {
+  if($(this).val() === "0"){
+    $(".t1.text_input").addClass("zero_input");
+  } else {
+    $(".t1.text_input").removeClass("zero_input");
+  }
+});
+
 
 $("form").on("input", function(event) {
-  let bill = Number($("bill_input").val());
-  let num_people = Number($("num_people_input").val());
-
+  calculate();
 });
+
+
+$(".reset_btn").on("click", function(event) {
+  $("#bill_input").val("");
+  $(".btn").removeClass("btn_clicked");
+  percentage = "";
+  $(".btn.btn_custom").val("");
+  $("#num_people_input").val("");
+
+
+  $(".tip").text("$0.00");
+  $(".total_bill").text("$0.00");
+});
+
+
+
+function calculate(){
+  const bill = Number($("#bill_input").val());
+  const num_people = Number($("#num_people_input").val());
+
+  const isBill = (bill !== 0) && (!Number.isNaN(bill));
+  const isPercentage = (percentage !== 0) && (!Number.isNaN(percentage));
+  const isNumPeople = (num_people !== 0) && (!Number.isNaN(num_people));
+
+  if(isBill && isPercentage && isNumPeople){
+    let splittedBill = bill / num_people;
+    let tip = splittedBill * (percentage/100);
+    let total = splittedBill + tip;
+
+    const tipString = "$" + tip.toFixed(2);
+    const totalString = "$" + total.toFixed(2);
+
+
+    $(".tip").text(tipString);
+    $(".total_bill").text(totalString);
+
+  }
+}
